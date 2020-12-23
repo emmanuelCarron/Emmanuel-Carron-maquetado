ce3fa40c46079cfbd4580c87f5e904bfb3ee94d3
Emmanuel <emmanuel.carron@itscordoba.edu.ar>
2020-12-23 00:24:15 -0300

    Maquetado con bootstrap y vue.js

---
+++ home.js
@@ -0,0 +1,38 @@
+const productos = new Vue({
+    el: '#productos',
+    data: {
+        productos: [
+            {nombre: 'PERFIL CONSTRUCCIÓN EN SECO',
+             detalle: 'MONTANTE 34 GALVANIZADO', 
+             dimensiones: '0.52MM X 34MM', 
+             imagen: 'images/producto.jpg',
+             precio: '0.00',
+             cant: '0',
+            },
+
+            {nombre: 'PERFIL CONSTRUCCIÓN EN SECO',
+             detalle: 'MONTANTE 35 GALVANIZADO MOLETEADO', 
+             dimensiones: '0.52MM X 35MM X 2600MM', 
+             imagen: 'images/producto.jpg',
+             precio: '0.00',
+             cant: '0',
+            },
+
+            {nombre: 'PERFIL CONSTRUCCIÓN EN SECO', 
+             detalle: 'MONTANTE 69 GALVANIZADO MOLETEADO', 
+             dimensiones: '0.52MM X 69MM X 2600MM', 
+             imagen: 'images/producto.jpg',
+             precio: '0.00',
+             cant: '0',
+            },
+
+            {nombre: 'PERFIL CONSTRUCCIÓN EN SECO', 
+             detalle: 'MONTANTE 70 GALVANIZADO MOLETEADO', 
+             dimensiones: '0.52MM X 70MM X 2600MM', 
+             imagen: 'images/producto.jpg',
+             precio: '0.00',
+             cant: '0',
+            },
+        ],
+    }
+})
