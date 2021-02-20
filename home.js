4540587c45de356a93c272e48089f9880a2c6460
Emmanuel <emmanuel.carron@itscordoba.edu.ar>
2021-02-20 19:52:13 -0300

    Añadido mostrar botón en card al hacer hover

--- home.js
+++ home.js
@@ -8,30 +8,34 @@ const productos = new Vue({
              imagen: 'images/producto.jpg',
              precio: '0.00',
              cant: '0',
+             hovered: false,
             },
-
+            
             {nombre: 'PERFIL CONSTRUCCIÓN EN SECO',
              detalle: 'MONTANTE 35 GALVANIZADO MOLETEADO', 
              dimensiones: '0.52MM X 35MM X 2600MM', 
              imagen: 'images/producto.jpg',
              precio: '0.00',
              cant: '0',
+             hovered: false,
             },
-
+        
             {nombre: 'PERFIL CONSTRUCCIÓN EN SECO', 
              detalle: 'MONTANTE 69 GALVANIZADO MOLETEADO', 
              dimensiones: '0.52MM X 69MM X 2600MM', 
              imagen: 'images/producto.jpg',
              precio: '0.00',
              cant: '0',
+             hovered: false,
             },
-
+    
             {nombre: 'PERFIL CONSTRUCCIÓN EN SECO', 
              detalle: 'MONTANTE 70 GALVANIZADO MOLETEADO', 
              dimensiones: '0.52MM X 70MM X 2600MM', 
              imagen: 'images/producto.jpg',
              precio: '0.00',
              cant: '0',
+             hovered: false,
             },
         ],
     }
